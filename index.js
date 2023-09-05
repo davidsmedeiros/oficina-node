5ed7df2f4f9972e9bc6c2e6d5c25eec40a9071e7
davidsmedeiros <david.medeiros@pagar.me>
2023-09-05 16:27:08 -0300

    feat: connect database with app

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const { Pool, Client } = require('pg');
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World 2!')
 })
+
+app.get('/testDB', (req, res) => {    
+    client.query('SELECT NOW()', (err, resp) => {
+        console.log("Error or response:: ", err, resp)     
+        res.send(resp)
+      });
+});
+
+const client = new Client({
+  user: 'user',
+  host: 'postgres',
+  database: 'db',
+  password: 'pass',
+  port: 5432,
+});
+client.connect()
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
